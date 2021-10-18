dd5e3724a37dd9f7ec63f426d33662f3d584df93
dearsip <pdaeisr@gmail.com>
2021-10-18 22:12:34 +0900

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,13 +14,6 @@ module.exports = {
         path: `${__dirname}/src`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/performers`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
     `gatsby-transformer-json`,
