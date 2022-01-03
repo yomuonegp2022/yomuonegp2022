d8f92e7f38d16b3eb4d362cbcf325b6fdbc4d921
yomuonegp2022 <92252007+yomuonegp2022@users.noreply.github.com>
2022-01-04 00:50:39 +0900

    更新情報ページ遷移挙動修正

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,7 +22,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const result = await graphql(`
     query {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
         edges {
           node {
             fields {
