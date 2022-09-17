678958f3353bced3511abea923b0407d20bfdfb9
yomuonegp2022 <yomuone2022@gmail.com>
2022-09-18 08:32:48 +0900

    おまけ微修正

--- src/pages/omake.js
+++ src/pages/omake.js
@@ -13,7 +13,7 @@ const StLink = styled(Link)`
 const Omake = () => (
   <Layout title="運営感想">
     <Seo title="運営感想" />
-    <p>2022年度読ム-１グランプリ主催が運営の感想をゆるく書いていきます。</p>
+    <p>2022年度読ム-１グランプリ主催が運営の感想とかをゆるく書いていきます。</p>
     <h3><StLink to="/omake1">はじめに</StLink></h3>
     <h3><StLink to="/omake2">サイトについて</StLink></h3>
     <h3><StLink to="/omake4">動画に関するルールについて</StLink></h3>
